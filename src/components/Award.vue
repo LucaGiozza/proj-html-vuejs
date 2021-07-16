142984c33cd35e0faff210b11f2416399153d21f
Luca Giozza <luca.giozza@gmail.com>
2021-07-16 22:10:36 +0200

    prima sezione main

---
+++ src/components/Award.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="contenitore column-6">
+                  <div>
+                      <p class="mt-5 free "> <strong>headche free</strong> </p>
+                      <h2 class="winning mt-2">Award Winning Moving Company</h2>
+                      <p class="lorem mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac augue. Orci varius natoque penatibus et magnis dis parturient montes, ridiculus mus. Aliquam enim leo, condimentum facilisis nulla sed, cursus arcu. Aliquam enim leo.</p>
+                      <p class="bott mt-4"> <strong>Read our testimonials</strong> </p>
+                                     
+                  </div>
+                  <div class="column-6">
+                      <div>
+    
+                      </div>
+
+                  </div>
+
+              </div>
+          </div>
+
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Award'
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container-fluid{
+    background-color:#e9e9e9;
+}
+.row{
+    display:flex;
+    justify-content: center;
+}
+.contenitore{
+    max-width:50%;
+    text-align: center;
+    .winning{
+        color:#69bf14;
+        
+        width: 39%;
+        margin-left: 31%;
+        text-align: center;
+        font-size: 36px;;
+    }
+    .free{
+        padding-top:100px;
+        color:rgb(124, 121, 121);
+        text-transform: uppercase;
+    }
+    
+
+}
+.lorem{
+    color:gray;
+}
+.bott{
+    padding:5px 10px;
+    background-color:#1975c0;
+    width:200px;
+    margin-left:37%;
+    border-radius: 22px; 
+    color:white;
+}
+.bott:hover{
+    filter:brightness(1.20)
+}
+
+
+</style>
--- src/components/HeaderInferiore.vue
+++ src/components/HeaderInferiore.vue
@@ -64,6 +64,7 @@ export default {
 }
 .container-fluid{
     height:500px;
+    overflow:hidden;
     
     padding:0;
     
