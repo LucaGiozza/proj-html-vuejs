9219fb356f78d437226448547d1aa0ec514af097
Luca Giozza <luca.giozza@gmail.com>
2021-07-17 11:17:45 +0200

    inizio terza sezione del main

--- src/components/Move.vue
+++ src/components/Move.vue
@@ -1,16 +1,37 @@
 <template>
   
-  <div>
-      
+  <div class="container-fluid">
+      <img src="@/assets/img/foto12.jpg" alt="">
+      <p>ciao</p>
+
   </div>
 </template>
 
 <script>
 export default {
+    name: 'Move'
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.container-fluid{
+    padding-top:150px;
+    height:600px;
+    position: relative;
+    
+    p{
+        position: absolute;
+        top:50%;
+        right: 30%;
+    }
+}
+img{
+    width:100%;
+    object-fit: cover;
+    height: 100%;
+    
+    
+}
 
 </style>
