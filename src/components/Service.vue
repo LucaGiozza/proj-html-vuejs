33782ac891be12027acee02d4aba3da0836fd86c
Luca Giozza <luca.giozza@gmail.com>
2021-07-17 10:50:52 +0200

    seconda sezione del main

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -17,20 +17,41 @@
           <div class="container-fluid d-flex">
               <div class="row">
                   <div class="column-4 first">
-                      <p>ciao</p>
+                      <img src="@/assets/img/foto16.jpg" alt="">
+                      
+                      <div class="descrizione">
+                          <h3><strong>Two man teams</strong> </h3>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+                      </div>
+                      
 
                   </div>
                   </div>
 
                    <div class="row">
                   <div class="column-4 first">
-                      <p>ciao</p>
+                      <img src="@/assets/img/foto24.jpg" alt="">
+                      <div class="descrizione">
+                          <h3> <strong>We do all the lifting</strong> </h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+                      </div>
+                      
 
                   </div>
                   </div>
                   <div class="row">
                   <div class="column-4 first">
-                      <p>ciao</p>
+                       <img src="@/assets/img/foto20.jpg" alt="">
+                       <div class="descrizione">
+                            <h3> <strong>Coast to coast</strong> </h3>
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+                       </div>
+                      
+
+                      
 
                   </div>
                   </div>
@@ -52,9 +73,10 @@ export default {
 
 <style lang="scss" scoped>
 .prof{
-    color:#7c7979;
+    
     text-transform: uppercase;
     padding-top:100px;
+    color:#afaeae;
 }
 .full{
     color: #69bf14;
@@ -64,8 +86,35 @@ export default {
     width:100%;
     padding-top:100px;
 }
+img{
+    
+    border:15px solid #e9e9e9;
+    
+
+        
+    
+}
 .first{
-    width: calc(100% / 3 )
+    margin-right:30px;
+    margin-left:-15px;
+    
+    
+    h3{
+        max-width: 90%;
+        padding:20px 0;
+        color:#1975c0;
+        text-align: center;
+        
+    }
+    p{
+        max-width: 80%;
+        margin:auto;
+    }
+    .descrizione{
+        border:15px solid #e9e9e9;
+        margin-right:30px;
+        
+    }
 }
 
 
